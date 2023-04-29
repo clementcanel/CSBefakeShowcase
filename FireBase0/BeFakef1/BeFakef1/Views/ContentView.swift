ff338ea3eb1de90b518d60c6a37f7b7d5a310dc1
Elijah Senior <112525161+NotLijee@users.noreply.github.com>
2023-04-29 15:52:50 -0700

    Add files via upload
    WIP on Final App. Create New Post changes made.

---
+++ FireBase0/BeFakef1/BeFakef1/Views/ContentView.swift
@@ -0,0 +1,27 @@
+
+//  ContentView.swift
+//  BeFakef1
+//
+//  Created by Elijah Senior on 4/17/23.
+
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var logStatus: Bool = false
+    // Tried to connect to login view using this
+    var body: some View {
+        if logStatus{
+            MainView()
+        }else{
+            LoginView(logStatus: $logStatus) // try this
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
