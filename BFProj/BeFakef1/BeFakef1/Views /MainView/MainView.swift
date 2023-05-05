ef8f92397c36f5b759181a915ad122cdc015293c
Elijah Senior <112525161+NotLijee@users.noreply.github.com>
2023-05-05 01:22:56 -0700

    Add files via upload

---
+++ BFProj/BeFakef1/BeFakef1/Views /MainView/MainView.swift
@@ -0,0 +1,35 @@
+//
+//  MainView.swift
+//  BeFakef1
+//
+//  Created by Elijah Senior on 4/17/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+//Supposed to connect after successful login
+    var body: some View {
+       
+            TabView{
+                PostsView()
+                    .tabItem {
+                        Image(systemName: "photo.fill.on.rectangle.fill")
+                        Text("posts")
+                    }
+                ProfileView()
+                    .tabItem {
+                        Image(systemName: "person.and.background.dotted")
+                        Text("profile")
+                    }
+            }
+            .tint(.gray)
+        }
+    }
+
+    struct MainView_Previews: PreviewProvider {
+        static var previews: some View {
+            MainView()
+        }
+    }
+
